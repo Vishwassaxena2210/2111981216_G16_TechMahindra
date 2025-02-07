9c08db87707ce5017b2406d7acb44c5a048680f4
Vishwassaxena2210 <vishwas1216.be21@chitkarauniversity.edu.in>
2025-02-07 15:29:03 +0530

    LEC3_07022025

---
+++ Lec3_07022025/index.js
@@ -0,0 +1,59 @@
+//let (block scoped) cannot be redeclare but can be reassigned.
+//in var we can redeclare global scope and also can reassigned the value.
+//always declare all the variables on the top.
+//types of loop: For, For Each, While, Do While, For in, For of
+//tempate literals ${i} from ejs
+//syntax of for each 
+/*
+arr=[1,2,3,4,5];
+arr.forEach(function(ele){
+    console.log(ele);
+    });
+
+//for in loops
+
+for(let i in arr){
+    console.log(i,arr[i]);
+    //arr[i] for element and i for index
+    
+}
+
+//for of loops
+
+for(let i of arr){
+    console.log(i);
+    //here i is value
+}
+
+//while
+
+let i=0;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+
+//do while(runs atleast one time)
+
+do{
+    console.log("g16");
+    
+}
+while(i<=10)
+    i++;
+*/
+////////////////////////////////////////////////////////////
+
+/*DOM Manipulation (DOM convert HTML code to a tree like structure)
+window is the parent of document
+ways to do: getElementById , getElementsByClassName , getElementByTagName
+other ways: querySelector : document.querySelector(".classname"/"#idname")
+document.querySelectorAll(".classname"/"#idname")
+
+innerText: gives spaces as well
+textContent: 
+*/
+
+
+
+
