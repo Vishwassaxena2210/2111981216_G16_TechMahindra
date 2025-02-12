789ab084058cbbc4603d36660f30ce40070f5d93
Vishwassaxena2210 <vishwas1216.be21@chitkarauniversity.edu.in>
2025-02-12 15:37:11 +0530

    Lec6_12022025

---
+++ Lec6/script.js
@@ -0,0 +1,33 @@
+// setTimeout interview questions
+// console.log(1);
+// console.log(sum(10,5));
+// function sum(x,y){
+// return x+y;
+// }
+// setTimeout(()=>{
+//     console.log(4);
+// },2000)
+// var start=1;
+// var end=100000000;
+// while(start<end){
+//     start++;
+
+// }
+// console.log("end");
+// setInterval(()=>{
+//     console.log(9);
+// },4000);
+function a(){
+    console.log("f1");
+    
+}
+
+
+var abc=function cdf(){
+    console.log("f2");
+}
+a();
+cdf();
+
+
+
